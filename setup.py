b46caee33af86cdbc54f75b66aab40de0c1e092f
grun <grunseid@gmail.com>
2018-07-08 15:04:46 -0700

    Add Python 3.6 to Travis and the classifiers in setup.py.

--- setup.py
+++ setup.py
@@ -102,6 +102,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
     install_requires=required,
