ab652d8add265a4e0b41fafbf4300cbac7bd4f71
Ansgar Grunseid <grunseid@gmail.com>
2018-08-30 11:56:21 -0700

    Correct variable name.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ class Publish(Command):
         os.system('python setup.py sdist bdist_wheel')
         sdist = 'dist/orderedmultidict-%s.tar.gz' % VERSION
         wheel = 'dist/orderedmultidict-%s-py2.py3-none-any.whl' % VERSION
-        rc = os.system('twine upload %s %s' % (egg, wheel))
+        rc = os.system('twine upload %s %s' % (sdist, wheel))
         sys.exit(rc)
 
 
